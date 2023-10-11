e90442dcbe1b5dc689e3c522ce4db7a1766c7453
LeeKyuSung <ks5050577@gmail.com>
2023-10-11 16:41:46 +0900

    [순수 JDBC]

--- src/main/java/com/example/hellospring/SpringConfig.java
+++ src/main/java/com/example/hellospring/SpringConfig.java
@@ -1,13 +1,19 @@
 package com.example.hellospring;
 
+import com.example.hellospring.repository.JdbcMemberRepository;
 import com.example.hellospring.repository.MemberRepository;
-import com.example.hellospring.repository.MemoryMemberRepository;
 import com.example.hellospring.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
+@RequiredArgsConstructor
 public class SpringConfig {
+    private final DataSource dataSource;
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +21,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
